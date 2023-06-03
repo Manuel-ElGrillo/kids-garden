5261ea20105daaa76ffbb6fe88e4f2b5f8ecf1ee
Diego.Corrales <docarquitectura@hotmail.com>
2023-06-03 15:39:24 -0300

    header parte2 y footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+        <h1>
+          ESTE ES EL HOME
+        </h1>
     </main>
   )
 }
