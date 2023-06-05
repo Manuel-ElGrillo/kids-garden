2da3e6c280fc56818d746ae694997f4267fd386a
Diego.Corrales <docarquitectura@hotmail.com>
2023-06-05 10:45:19 -0300

    Login parte01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
 import Image from 'next/image'
+import React, { useState } from 'react';
+  
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalToggle = () => {
+    setShowModal(!showModal);
+  };
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
         <h1>
-          ESTE ES EL HOME
+          este es el home
         </h1>
+      </div>
     </main>
   )
 }
