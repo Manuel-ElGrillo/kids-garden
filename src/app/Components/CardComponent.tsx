0c37b1b39f02cc81ce65f29c373061406467ee8e
Manuel De Leon <manuel@deleonnet.com>
2023-06-11 18:59:25 -0300

    flip card animation added

--- src/app/Components/CardComponent.tsx
+++ src/app/Components/CardComponent.tsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import Link from 'next/link'
 import Image from 'next/image'
 import { Cards } from '../Interfaces/Cards'
+import styles from "../styles/Cards.module.css"
 
-const CardComponent: React.FC<Cards> = ({title, text, link, imgSrc, alt, bg}) => {
+const CardComponent: React.FC<Cards> = ({title, text, imgSrc, alt, bg, backText, backTitle}) => {
   return (
-    <div className={bg}>
-      <h3 className='mb-2 text-center text-2xl font-bold tracking-tight text-white'>{title}</h3>
-      <p className='mb-3 font-normal text-black text-center'>{text}</p>
-      <div className='block text-center'>
-        <Link href={link} className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-blue-500 bg-white rounded-lg'>+ Info</Link>
+    <div className={styles["card-flip"]}>
+
+      {/* Cara frontal */}
+      <div className={bg}>
+        <h3 className='mb-2 text-center text-2xl font-bold tracking-tight text-white'>{title}</h3>
+        <p className='mb-3 font-normal text-black text-center'>{text}</p>
+        <div className='block text-center'>
+        </div>
+        <div className='flex justify-center pt-5'>
+          <Image src={imgSrc} alt={alt} width={120} height={120}></Image>
+        </div>
       </div>
-      <div className='flex justify-center pt-5'>
-        <Image src={imgSrc} alt={alt} width={120} height={120}></Image>
+
+      {/* Cara trasera */}
+      <div className={bg}>
+        <h3 className='mb-2 text-center text-2xl font-bold tracking-tight text-white'>{backTitle}</h3>
+        <p className='mb-3 font-normal text-black text-center'>{backText}</p>
       </div>
     </div>
+
   )
 }
 
 export default CardComponent
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,34 +12,38 @@ export default function Home() {
         <CardComponent
           title='Estrellitas'
           text={<>Desarrollo temprano de los niños de 45 días a 2 años</>}
-          link='/nosotros'
           imgSrc='/img/rings.png'
           alt='Imagen de juguete para niños'
-          bg='bg-[#1999FA] max-w-sm p-6 rounded-lg shadow basis-1/4 mx-4' />
+          bg='bg-[#1999FA] max-w-[300px] p-6 rounded-lg shadow space-x-0.5'
+          backTitle='Estrellita'
+          backText='En esta etapa desarrollamos la habilidad de comunicarse libremente y expresar sus primeras palabras' />
 
         <CardComponent
           title='Pequeñitos'
           text={<>Actividades para <br /> niños a partir de 2 años</>}
-          link='/nosotros'
           imgSrc='/img/boat.png'
           alt='Imagen de un barco de juguete'
-          bg='bg-[#9DC620] max-w-sm p-6 rounded-lg shadow basis-1/4 mx-4' />
+          bg='bg-[#9DC620] max-w-[300px] p-6 rounded-lg shadow space-x-0.5' 
+          backTitle='Pequeñitos'
+          backText='En esta etapa potenciamos las habilidades de interacción mediante actividades grupales'/>
 
         <CardComponent
           title='Primeros Pasos'
           text={<>Actividades para niños <br /> a partir de 3 años</>}
-          link='/nosotros'
           imgSrc='/img/blocks.png'
           alt='Imagen de cubos de juguete'
-          bg='bg-[#FF7223] max-w-sm p-6 rounded-lg shadow basis-1/4 mx-4' />
+          bg='bg-[#FF7223] max-w-[300px] p-6 rounded-lg shadow space-x-0.5' 
+          backTitle='Primeros Pasos'
+          backText='En esta etapa brindamos las primeras experiencias fuera de la Institución'/>
 
         <CardComponent
           title='Pequeños Gigantes'
           text={<>Grupo para niños <br /> de 4 a 6 años</>}
-          link='/nosotros'
           imgSrc='/img/children.png'
           alt='Imagenes de niños de caricatura'
-          bg='bg-[#C81FA0] max-w-sm p-6 rounded-lg shadow basis-1/4 mx-4' />
+          bg='bg-[#C81FA0] max-w-[300px] p-6 rounded-lg shadow space-x-0.5'
+          backTitle='Pequeños Gigantes'
+          backText='En esta etapa el eje esta centrado en las habilidades grupales y la inclusión didáctica ' />
 
       </section>
 
